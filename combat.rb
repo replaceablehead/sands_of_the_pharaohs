084067bfe86110ed71efbbc8e30632dbfe61e1d6
Peter Grace <pdranken@gmail.com>
2026-06-01 21:06:02 +1000

    add combat classes and tests

--- combat.rb
+++ combat.rb
@@ -1,2 +1,52 @@
-class Combat
+module Combat
+  def self.attack_roll(attacker)
+    attacker.str + rand(1..6)
+  end
+
+  def self.defence_roll(attacker)
+    attacker.dex + rand(1..6)
+  end
+  def self.resolve_attack(attacker, defender)
+    potential_damage = attack_roll(attacker)
+    defence_score = defence_roll(attacker)
+    hit_chance = rand(100)
+
+    if defence_score < defender.dex
+      # defender wins the defence roll
+      if hit_chance < 10
+        # 10% chance full damage happens
+        apply_damage(defender, potential_damage)
+        :hit
+      elsif hit_chance < 55
+        # 45% chance of half damage
+        apply_damage(defender, potential_damage / 2)
+        :partial
+      else
+        # 45% chance to miss dealing no damage
+        :miss
+      end
+    else
+      # attacker wins the defence roll
+      if hit_chance < 90
+        # 90% chance full damage occurs
+        apply_damage(defender, potential_damage)
+        :hit
+      elsif hit_chance < 95
+        # 5% chance half damage occurs
+        apply_damage(defender, potential_damage / 2)
+        :partial
+      else
+        # 5% chance to miss entirely
+        :miss
+      end
+    end
+  end
+  def self.apply_damage(defender, damage)
+    defender.hp -= damage
+    defender.hp = 0 if defender.hp < 0
+  end
+
+  def self.dead?(entity)
+    entity.hp <= 0
+  end
 end
--- test/combat_test.rb
+++ test/combat_test.rb
@@ -1,10 +1,68 @@
 require 'minitest/autorun'
-require_relative '../combat' # load the combat file for testing
+require_relative '../combat'
+require_relative '../character'
+require_relative '../npc'
 
-# test tries to create a new combat instance and checks if it was created
 class CombatTest < Minitest::Test
-  def test_combat_initializes
-    combat = Combat.new
-    assert_instance_of Combat, combat
+  def setup
+   
+    @player = Character.new
+    @rat = Rat.new
+  end
+
+  # test attack roll is inside the expected valuies
+  def test_attack_roll_within_range
+    result = Combat.attack_roll(@player)
+    assert result >= @player.str + 1
+    assert result <= @player.str + 6
+  end
+
+  # test defence roll is inside expected values
+  def test_defence_roll_within_range
+    result = Combat.defence_roll(@player)
+    assert result >= @player.dex + 1
+    assert result <= @player.dex + 6
+  end
+
+  # check the damage doesn't go below zero
+  def test_hp_never_below_zero
+    @rat.hp = 1
+    Combat.apply_damage(@rat, 999)
+    assert_equal 0, @rat.hp
+  
+  end
+
+  # test damage reduces hp
+  def test_apply_damage_reduces_hp
+    starting_hp = @rat.hp
+    Combat.apply_damage(@rat, 5)
+    assert_equal starting_hp - 5, @rat.hp
+  end
+
+  # test dead check returns true when hp goes to zero
+  def test_dead_when_hp_zero
+    @rat.hp = 0
+    assert Combat.dead?(@rat)
+  end
+
+  # test dead check returns false when hp is above zero
+  def test_not_dead_when_hp_above_zero
+    @rat.hp = 10
+    refute Combat.dead?(@rat)
+  end
+
+  # test resolve attack returns a symbol
+  def test_resolve_attack_returns_valid_outcome
+    result = Combat.resolve_attack(@player, @rat)
+    assert [:hit, :partial, :miss].include?(result)
+  end
+
+
+  # test damage never exceeds maximum ammount
+  def test_damage_never_exceeds_maximum
+    starting_hp = @rat.hp
+    Combat.resolve_attack(@player, @rat)
+    max_possible_damage = @player.str + 6
+    assert @rat.hp >= starting_hp - max_possible_damage
   end
 end
