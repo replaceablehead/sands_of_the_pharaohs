e3cd0fa1d04944d9f68a8d473e40dfd9e510e898
Peter Grace <pdranken@gmail.com>
2026-06-01 20:21:01 +1000

    add tests for npcs and place holder npc classes

--- npc.rb
+++ npc.rb
@@ -1,2 +1,79 @@
 class Npc
+  attr_accessor :hp, :str, :dex, :int, :wis, :x, :y, :type
+  end
+end
+
+class Rat < Npc
+  def initialize
+    super
+    @type = :rat
+    @hp = 10
+    @str = 3
+    @dex = 8
+    @int = 1
+    @wis = 1
+    # set some place holder coodinates
+    @x = 0
+    @y = 0
+  end
+end
+
+class Mummy < Npc
+  def initialize
+    super
+    @type = :mummy
+    @hp = 30
+    @str = 8
+    @dex = 3
+    @int = 2
+    @wis = 2
+     # set some place holder coodinates
+    @x = 0
+    @y = 0
+  end
+end
+
+class AnubisGuardLight < Npc
+  def initialize
+    super
+    @type = :anubis_guard_light
+    @hp = 50
+    @str = 12
+    @dex = 10
+    @int = 5
+    @wis = 5
+     # set some place holder coodinates
+    @x = 0
+    @y = 0
+  end
+end
+
+class AnubisGuardDark < Npc
+  def initialize
+    super
+    @type = :anubis_guard_dark
+    @hp = 70
+    @str = 15
+    @dex = 12
+    @int = 8
+    @wis = 8
+     # set some place holder coodinates
+    @x = 0
+    @y = 0
+  end
+end
+
+class CobraBoss < Npc
+  def initialize
+    super
+    @type = :cobra_boss
+    @hp = 150
+    @str = 20
+    @dex = 15
+    @int = 10
+    @wis = 10
+     # set some place holder coodinates
+    @x = 0
+    @y = 0
+  end
 end
--- test/npc_test.rb
+++ test/npc_test.rb
@@ -1,10 +1,83 @@
 require 'minitest/autorun'
-require_relative '../npc' # load the npc file for testing
+require_relative '../npc'
 
-# test tries to create a new npc and checks if a new npc was created
 class NpcTest < Minitest::Test
-  def test_npc_initializes
-    npc = Npc.new
-    assert_instance_of Npc, npc
+  # test tries to create each npc type and checks it was created
+  def test_rat_initializes
+    npc = Rat.new
+    assert_instance_of Rat, npc
+  end
+
+  def test_mummy_initializes
+    npc = Mummy.new
+    assert_instance_of Mummy, npc
+  end
+
+  def test_anubis_guard_light_initializes
+    npc = AnubisGuardLight.new
+    assert_instance_of AnubisGuardLight, npc
+  end
+
+  def test_anubis_guard_dark_initializes
+    npc = AnubisGuardDark.new
+    assert_instance_of AnubisGuardDark, npc
+  end
+
+  def test_cobra_boss_initializes
+    npc = CobraBoss.new
+    assert_instance_of CobraBoss, npc
+  end
+
+  # test checks each npc type has correct default stats
+  def test_rat_has_correct_stats
+    npc = Rat.new
+    assert_equal 10, npc.hp
+    assert_equal 3, npc.str
+    assert_equal 8, npc.dex
+    assert_equal 1, npc.int
+    assert_equal 1, npc.wis
+  end
+
+  def test_mummy_has_correct_stats
+    npc = Mummy.new
+    assert_equal 30, npc.hp
+    assert_equal 8, npc.str
+    assert_equal 3, npc.dex
+    assert_equal 2, npc.int
+    assert_equal 2, npc.wis
+  end
+
+  def test_anubis_guard_light_has_correct_stats
+    npc = AnubisGuardLight.new
+    assert_equal 50, npc.hp
+    assert_equal 12, npc.str
+    assert_equal 10, npc.dex
+    assert_equal 5, npc.int
+    assert_equal 5, npc.wis
+  end
+
+  def test_anubis_guard_dark_has_correct_stats
+    npc = AnubisGuardDark.new
+    assert_equal 70, npc.hp
+    assert_equal 15, npc.str
+    assert_equal 12, npc.dex
+    assert_equal 8, npc.int
+    assert_equal 8, npc.wis
+  end
+
+  def test_cobra_boss_has_correct_stats
+    npc = CobraBoss.new
+    assert_equal 150, npc.hp
+    assert_equal 20, npc.str
+    assert_equal 15, npc.dex
+    assert_equal 10, npc.int
+    assert_equal 10, npc.wis
+  end
+
+  # test checks all npcs have a spawn points
+  def test_npc_has_starting_position
+    npc = Rat.new
+    assert_equal 0, npc.x
+    assert_equal 0, npc.y
   end
 end
