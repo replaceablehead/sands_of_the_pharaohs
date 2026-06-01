d8d5d6d075622691503338c4ace28d3052a14349
Peter Grace <pdranken@gmail.com>
2026-06-01 23:15:41 +1000

    add player sprite to map

--- character.rb
+++ character.rb
@@ -1,14 +1,22 @@
-# basic placeholder character with default stats
-class Character
-  attr_accessor :hp, :str, :dex, :int, :wis, :x, :y
+require 'gosu'
 
-  def initialize
+# creates a character class
+class Character
+  attr_accessor :hp, :str, :dex, :int, :wis, :x, :y, :image
+  TILE_SIZE = 32
+
+  def initialize(x = 0, y = 0)
     @hp = 100
     @str = 10
     @dex = 10
     @int = 10
     @wis = 10
-    @x = 0
-    @y = 0
+    @x = x
+    @y = y
+    @image = Gosu::Image.new('assets/character/rotations/south.png')
+  end
+
+  def draw
+    @image.draw(@x * TILE_SIZE, @y * TILE_SIZE, 1)
   end
 end
--- main.rb
+++ main.rb
@@ -8,17 +8,22 @@ require_relative 'data_loader'
 
 class Game < Gosu::Window
   def initialize
-    super 800, 600
-    self.caption = "Sands of the Pharaohs"
-    @map = Map.new('maps/level1.tmj')
-  end
-
-  def update
-  end
-
-  def draw
-    @map.draw
-  end
+  super 960, 600
+  self.caption = "Sands of the Pharaohs"
+  @map = Map.new('maps/level1.tmj')
+  
+  # read spawn point from map and convert to tile coordinates
+  map_data = JSON.parse(File.read('maps/level1.tmj'))
+  spawn = DataLoader.find_object(map_data, 'player_spawn', 'player_spawn')
+  spawn_x = (spawn['x'] / 32).floor
+  spawn_y = (spawn['y'] / 32).floor
+  
+  @player = Character.new(spawn_x, spawn_y)
 end
 
+def draw
+  @map.draw
+  @player.draw
+end
+end
 Game.new.show if __FILE__ == $0
