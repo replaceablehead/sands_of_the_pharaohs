42a4020a45895477a70f3fb051416600a8645d10
Peter Grace <pdranken@gmail.com>
2026-06-01 19:44:24 +1000

    Add spawn point test to map_test

--- test/map_test.rb
+++ test/map_test.rb
@@ -1,14 +1,19 @@
 require 'minitest/autorun'
-require_relative '../map'
-
-class FakeWindow
-  # fake window as stand in for gosu-tiled to run without opening a real window
-end
+require 'json'
 
 class MapTest < Minitest::Test
   def test_map_loads_correct_dimensions
-    map = Map.new(FakeWindow.new, 'maps/level1.tmj')
-    assert_equal 20, map.width
-    assert_equal 16, map.height
+    data = JSON.parse(File.read('maps/level1.tmj'))
+    assert_equal 20, data['width']
+    assert_equal 16, data['height']
   end
+end
+
+# test and see if the map loads the spawn point
+def test_map_spawn_point
+  data = JSON.parse(File.read('maps/level1.tmj'))
+  spawn_layer = data['layers'].find { |l| l['name'] == 'player_spawn' }
+  spawn_point = spawn_layer['objects'].find { |o| o['name'] == 'player_spawn' }
+  assert_equal 318.0, spawn_point['x']
+  assert_equal 467.0, spawn_point['y']
 end
